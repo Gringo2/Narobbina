24026b4c357b23535185cfe9268135a3bcd52fb1
Gringo2 <jobs.bezu@gmail.com>
2024-02-13 00:15:59 -0800

    stagin--cont

--- assets/js/shadow.js
+++ assets/js/shadow.js
@@ -452,7 +452,7 @@ customElements.define('services-component', class extends HTMLElement {
 
         <div class="row">
           <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-up">
-            <div class="card" style="background-image: url(assets/img/values-1.jpg);">
+            <div class="card" style="background-image: url(assets/img/security.jpg);">
               <div class="card-body">
                 <h5 class="card-title"><a href="">Security services</a></h5>
                 <p class="card-text">Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod tempor ut labore et dolore magna aliqua.</p>
@@ -461,7 +461,7 @@ customElements.define('services-component', class extends HTMLElement {
             </div>
           </div>
           <div class="col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="fade-up" data-aos-delay="100">
-            <div class="card" style="background-image: url(assets/img/values-2.jpg);">
+            <div class="card" style="background-image: url(assets/img/safety.jpg);">
               <div class="card-body">
                 <h5 class="card-title"><a href="">Safety services</a></h5>
                 <p class="card-text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem doloremque laudantium, totam rem.</p>
@@ -471,7 +471,7 @@ customElements.define('services-component', class extends HTMLElement {
 
           </div>
           <div class="col-md-6 d-flex align-items-stretch mt-4" data-aos="fade-up" data-aos-delay="200">
-            <div class="card" style="background-image: url(assets/img/values-3.jpg);">
+            <div class="card" style="background-image: url(assets/img/access.jpg);">
               <div class="card-body">
                 <h5 class="card-title"><a href="">Access control system</a></h5>
                 <p class="card-text">Nemo enim ipsam voluptatem quia voluptas sit aut odit aut fugit, sed quia magni dolores.</p>
@@ -480,7 +480,7 @@ customElements.define('services-component', class extends HTMLElement {
             </div>
           </div>
           <div class="col-md-6 d-flex align-items-stretch mt-4" data-aos="fade-up" data-aos-delay="300">
-            <div class="card" style="background-image: url(assets/img/values-4.jpg);">
+            <div class="card" style="background-image: url(assets/img/Networking.jpg);">
               <div class="card-body">
                 <h5 class="card-title"><a href="">Satellite TV networking</a></h5>
                 <p class="card-text">Nostrum eum sed et autem dolorum perspiciatis. Magni porro quisquam laudantium voluptatem.</p>
