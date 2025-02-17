b8e44c78457bf66da5b032564fec548a1a36d43e
Gringo2 <jobs.bezu@gmail.com>
2025-02-18 01:35:53 +0300

    slider--fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -290,9 +290,6 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
-  
-
-
   /**
    * Initiate pURE cOUNTER
    */
@@ -311,6 +308,33 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   window.addEventListener('load', () => {
     aos_init();
+  });
+
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    autoplay: {
+      delay: 1000, // 1 second
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+    },
   });
 
 });
